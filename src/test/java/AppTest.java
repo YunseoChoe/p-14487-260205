be5363158179c131b091274d974ce54fb983470d
choiyunseo <choeyunseo4060@naver.com>
2026-02-05 11:34:18 +0900

    Feat: 10+20=30 테이스케이스 통과

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,13 +1,24 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
     @Test
+    @DisplayName("1 + 2 = 3")
     void t1() {
         App app = new App();
         int rst = app.plus(1, 2);
 
         assertThat(rst).isEqualTo(3); // rst의 결과는 3 예상
+    }
+
+    @Test
+    @DisplayName("10 + 20 = 30")
+    void t2() {
+        App app = new App();
+        int rst = app.plus(10, 20);
+
+        assertThat(rst).isEqualTo(30);
     }
 }
