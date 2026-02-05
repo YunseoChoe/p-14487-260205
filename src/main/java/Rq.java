df842250ad66764790925a07910ccc1bb1c2545c
choiyunseo <choeyunseo4060@naver.com>
2026-02-05 12:14:09 +0900

    Feat: RqTest Red, Green, Blue 구현

---
+++ src/main/java/Rq.java
@@ -0,0 +1,12 @@
+public class Rq {
+
+    private String cmd;
+
+    public Rq(String cmd) {
+        this.cmd = cmd;
+    }
+
+    public String getActionName() {
+        return cmd.split("\\?")[0]; // 리팩토링
+    }
+}
